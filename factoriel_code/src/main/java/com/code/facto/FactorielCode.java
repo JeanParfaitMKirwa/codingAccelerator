04e5038ff282c18aa0811855b8454850476b9f78
training <jean.parfait.kirwa@gmail.com>
2020-03-05 11:20:15 +0100

    factoriel

---
+++ factoriel_code/src/main/java/com/code/facto/FactorielCode.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.code.facto;
+
+/**
+ * @author training
+ *
+ */
+public class FactorielCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int argument = Integer.parseInt(args[0]);
+		int result = 1;
+		for (int i = argument; i > 0 ; i--) {
+			if (i == 1) {
+				result = result * 1 ;
+			} else {
+				result = result * i;
+			}
+			
+		}
+		System.out.println(result);
+	}
+
+}
