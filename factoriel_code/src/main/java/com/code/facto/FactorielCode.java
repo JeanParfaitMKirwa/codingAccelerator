9b481d5add047c4d5b055c953cce70b3d59ae45d
training <jean.parfait.kirwa@gmail.com>
2020-03-22 16:58:02 +0100

    Factoriel retour, sur Java des entiers ne calcule pas des grands entiers, changement faite  ici on utilise des "long" pour claculer sur les grands entiers

--- factoriel_code/src/main/java/com/code/facto/FactorielCode.java
+++ factoriel_code/src/main/java/com/code/facto/FactorielCode.java
@@ -15,7 +15,7 @@ public class FactorielCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int argument = Integer.parseInt(args[0]);
-		int result = 1;
+		long result = 1;
 		for (int i = argument; i > 0 ; i--) {
 			if (i == 1) {
 				result = result * 1 ;
